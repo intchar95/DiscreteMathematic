047f979147399ef1152cb7e19991e981f06e5f3f
intchar95 <55632536+intchar95@users.noreply.github.com>
2019-09-29 03:35:54 +0900

    2sanAssignment02 - 4

---
+++ 20175327_02.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main(int argc, char *argv[]){
+	int i,sum=0;
+	
+	
+	if(argc <= 201){
+		 for(i=0;i<argc/2;i++){
+			
+			if(strcmp(argv[2*i+1],"+") == 0){
+				
+				sum += atoi(argv[2*i+2]);
+				
+			}else{
+			
+				sum -=  atoi(argv[2*i+2]);
+				
+			}
+		} 
+	}else{
+		printf("연산은 최대 100개까지 가능합니다.");
+	}
+	printf("%d\n\n\n",sum);
+
+	return 0;
+}
