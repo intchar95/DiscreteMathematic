550f1805adaee52913ded6fddd8c2402137d6bfa
intchar95 <55632536+intchar95@users.noreply.github.com>
2019-10-10 04:04:32 +0900

    2sanAssignment03 - 2

--- 20175327_03.c
+++ 20175327_03.c
@@ -2,17 +2,22 @@
 
 void main(void){
 
-    int i, j;
+    int i, j,num=0;
 
     char arr[4] = { 'a', 'b', 'c', 'd' };
 
     int n = 4;
-	int k;
+	int k=1;
 
     for (i = 1; i < 1<<n; i++){
 
         for (j = 0; j < n; j++){
 		
+		if(num==0){
+			k++;
+			printf("[%04d]",k);
+			num=1;
+		}
             if (i & (1 << j)){
 			
                 printf("%c ",arr[j]);
@@ -20,10 +25,8 @@ void main(void){
             }
 
         }
-
+		num=0;
         printf("\n");
 
     }
-
-	printf("%d",1<<n);
 }
