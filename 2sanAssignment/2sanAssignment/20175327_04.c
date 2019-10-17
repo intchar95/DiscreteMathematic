982de5f25c05c8a30d2eb755921c39d3a790cdd3
intchar95 <55632536+intchar95@users.noreply.github.com>
2019-10-17 23:19:29 +0900

    2sanAssignment04 - 4

--- 2sanAssignment/2sanAssignment/20175327_04.c
+++ 2sanAssignment/2sanAssignment/20175327_04.c
@@ -1,33 +1,33 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <locale.h>
 #include <string.h>
 #include <io.h>
 #include <fcntl.h>
-#include <wchar.h>
 #define _CRT_SECURE_NO_WARNINGS
 
 void main(int argc, char *argv[]) {
 	errno_t err1,err2;
 	FILE *fp1, *fp2;
-	char str[500];
 	int i, j, num;
 	int k = 2;
 
-	_setmode(_fileno(stdout), _O_U16TEXT);
 
+//	setlocale(LC_ALL, "Korean");
 	if ((err1 = fopen_s(&fp1, argv[0], "r")) != 0) {
-		printf(fp1, "fail to open file.");
+		printf("fail to open file.");
 		return ;
 	}
 	
 	if ((err2 = fopen_s(&fp2, argv[1], "wt")) != 0) {
-		printf(fp2, "fail to create file for write.");
+		fprintf(fp2, "fail to create file for write.");
 		return ;
 	}
-	
-	fprintf(fp2, "[0001]");
-	fwprintf(fp2, L"%c \n", 155);
 
+
+;
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	fwprintf(fp2, L"[0001]");
+	fwprintf(fp2, L"%c \n", 155);
 	for (i = 1; i < 1 << argc - 2; i++) {
 		num = 0;
 		fprintf(fp2, "[%04d]", k);
@@ -56,7 +56,7 @@ void main(int argc, char *argv[]) {
 		"   student ID : 20175327\n\n"
 		"@)!&%#@&@)!&%#@&@)!&%#@&@)!&%#@&@)!&%#@&\n");
 
-	//	fclose(fp1);
+	fclose(fp1);
 	fclose(fp2);
 
 }
