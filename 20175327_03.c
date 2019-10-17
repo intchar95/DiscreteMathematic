df72d7b436a460a6a2341fed8b9611798bd2adab
intchar95 <55632536+intchar95@users.noreply.github.com>
2019-10-17 17:25:58 +0900

    2sanAssignment04 - 1

--- 20175327_03.c
+++ 20175327_03.c
@@ -4,31 +4,38 @@
 
 void main(int argc, char *argv[]){
 
-    int i, j, num=0;
-	int k=1;
+    int i, j, num;
+	int k=2;
 	
 	system("chcp 437");
 	
 	printf("\n[0001]%c\n",155);
 	
     for (i = 1; i < 1<<argc-1; i++){
+		num = 0;
+		printf("[%04d]", k);
+		k++;
 
         for (j = 0; j < argc-1; j++){
-		
-		if(num==0){
-			k++;
-			printf("[%04d]",k);
-			num=1;
-		}
-            if (i & (1 << j)){
-			
-                printf("%s ",argv[j+1]);
 
-            }
+			if (i & (1 << j)) {
+
+				if (num == 1)
+					printf(", ");
+
+				printf("%s", argv[j + 1]);
+				num = 1;
+			}
 
         }
-		num=0;
+		
         printf("\n");
 
     }
+	
+	printf("\n");
+	printf( "@)!&%#@&@)!&%#@&@)!&%#@&@)!&%#@&@)!&%#@&\n\n"
+			"   Yong-Beom Cho\n"
+			"   student ID : 20175327\n\n"
+			"@)!&%#@&@)!&%#@&@)!&%#@&@)!&%#@&@)!&%#@&\n");
 }
---
+++ 2sanAssignment/2sanAssignment/20175327_04.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+main4(void) {
+
+	printf("gg");
+}
